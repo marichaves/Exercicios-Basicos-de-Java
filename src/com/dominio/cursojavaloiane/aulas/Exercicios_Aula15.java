e85bedf0bffca2643a1558a7c9f760dda6e15da9
Mariana Chaves <79344040+marichaves@users.noreply.github.com>
2023-05-26 12:03:38 -0300

    Update

--- src/com/dominio/cursojavaloiane/aulas/Exercicios_Aula15.java
+++ src/com/dominio/cursojavaloiane/aulas/Exercicios_Aula15.java
@@ -254,36 +254,63 @@ public class Exercicios_Aula15 {
 		
 		
 		
-		System.out.println("Digite a primeira nota no aluno: ");
-		double nota1 = scan.nextDouble();
-		System.out.println("Digite a segunda nota no aluno: ");
-		double nota2 = scan.nextDouble();
-
-		double media = (nota1 + nota2) / 2;
-		String aproveitamento = "";
-		if(media >= 9 && media <= 10) {
-			aproveitamento = "A";
-		} else if(media >= 7.5 && media < 9) {
-			aproveitamento = "B";
-		}  else if(media >= 6 && media < 7.5) {
-			aproveitamento = "C";
-		} else if(media >= 4 && media < 6) {
-			aproveitamento = "E";
-		} else if(media >= 0 && media < 4) {
-			aproveitamento = "E";
-		}
-		System.out.println("Nota1: " + nota1);
-		System.out.println("Nota2: " + nota2);
-		System.out.println("Media: " + media);
-		System.out.println("Conceito: " + aproveitamento);
+//		System.out.println("Digite a primeira nota no aluno: ");
+//		double nota1 = scan.nextDouble();
+//		System.out.println("Digite a segunda nota no aluno: ");
+//		double nota2 = scan.nextDouble();
+//
+//		double media = (nota1 + nota2) / 2;
+//		String aproveitamento = "";
+//		if(media >= 9 && media <= 10) {
+//			aproveitamento = "A";
+//		} else if(media >= 7.5 && media < 9) {
+//			aproveitamento = "B";
+//		}  else if(media >= 6 && media < 7.5) {
+//			aproveitamento = "C";
+//		} else if(media >= 4 && media < 6) {
+//			aproveitamento = "E";
+//		} else if(media >= 0 && media < 4) {
+//			aproveitamento = "E";
+//		}
+//		System.out.println("Nota1: " + nota1);
+//		System.out.println("Nota2: " + nota2);
+//		System.out.println("Media: " + media);
+//		System.out.println("Conceito: " + aproveitamento);
+//		
+//		switch(aproveitamento) {
+//		case "A":
+//		case "B":
+//		case "C": System.out.println("APROVADO"); break;
+//		case "D":
+//		case "E": System.out.println("REPROVADO"); break;
+//		}
 		
-		switch(aproveitamento) {
-		case "A":
-		case "B":
-		case "C": System.out.println("APROVADO"); break;
-		case "D":
-		case "E": System.out.println("REPROVADO"); break;
-		}
+		
+//		System.out.println("Entre com o lado 1: ");
+//		int lado1 = scan.nextInt();
+//		System.out.println("Entre com o lado 2: ");
+//		int lado2 = scan.nextInt();
+//		System.out.println("Entre com o lado 3: ");
+//		int lado3 = scan.nextInt();
+//		
+//		
+//		if (((lado1 + lado2) > lado3) || 
+//				((lado1 + lado3) > lado2) ||
+//				((lado2 + lado3) > lado1)) {
+//			if (lado1 == lado2 && lado1 == lado3) {
+//				System.out.println("Triângulo Equilátero");
+//			}else if (lado1 != lado2 && lado1 != 3 && lado3 != lado2 ){
+//				System.out.println("Triângulo Escaleno");
+//			} else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+//				System.out.println("Triângulo Isóceles");
+//			}
+//			
+//		} else {
+//			System.out.println("Não forma um triângulo.");
+//		}
+		
+		
+		
 		
 	}
 
